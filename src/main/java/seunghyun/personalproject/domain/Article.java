a99bea0a415130eba11456534c6153ffbb446e0f
1winhyun <jimking1@naver.com>
2024-12-21 15:24:14 +0900

    feat: 프로젝트 시작

---
+++ src/main/java/seunghyun/personalproject/domain/Article.java
@@ -0,0 +1,36 @@
+package seunghyun.personalproject.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@Table(name = "Article")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="content")
+    private String content;
+
+
+}
