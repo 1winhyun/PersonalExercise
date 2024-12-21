624f4563f95492faf3968bd949cdad4872fc3586
1winhyun <jimking1@naver.com>
2024-12-21 17:15:40 +0900

    feat: 게시물 수정, 삭제, 단건 조회 구현

--- src/main/java/seunghyun/personalproject/controller/PostController.java
+++ src/main/java/seunghyun/personalproject/controller/PostController.java
@@ -5,9 +5,13 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import seunghyun.personalproject.domain.Article;
 import seunghyun.personalproject.dto.PostRequestDTO;
@@ -16,17 +20,18 @@ import seunghyun.personalproject.service.PostService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/articles")
 public class PostController {
     private final PostService postService;
 
-    @PostMapping("/api/articles")
+    @PostMapping
     public ResponseEntity<Article> create(@RequestBody PostRequestDTO requestDTO){
         Article post=postService.save(requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(post);
     }
 
-    @GetMapping("api/articles")
+    @GetMapping
     public ResponseEntity<List<PostResponseDTO>>findAllPosts(){
         List<PostResponseDTO>posts=postService.findAll()
                 .stream()
@@ -34,5 +39,26 @@ public class PostController {
                 .collect(Collectors.toList());
         return ResponseEntity.ok()
                 .body(posts);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponseDTO>findPostById(@PathVariable Long id){
+        Article post=postService.findById(id);
+        return ResponseEntity.ok()
+                .body(PostResponseDTO.from(post));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void>deletePostById(@PathVariable Long id){
+        postService.deleteById(id);
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody PostRequestDTO requestDTO){
+        Article post=postService.update(requestDTO,id);
+        return ResponseEntity.ok()
+                .body(post);
     }
 }
--- src/main/java/seunghyun/personalproject/service/PostService.java
+++ src/main/java/seunghyun/personalproject/service/PostService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import seunghyun.personalproject.domain.Article;
 import seunghyun.personalproject.dto.PostRequestDTO;
+import seunghyun.personalproject.dto.PostResponseDTO;
 import seunghyun.personalproject.repository.PostRepository;
 
 @Service
@@ -13,10 +14,28 @@ public class PostService {
     private final PostRepository postRepository;
 
     public Article save(PostRequestDTO requestDTO){
-        return postRepository.save(requestDTO.make());
+        Article post=requestDTO.make();
+        return postRepository.save(post);
     }
 
     public List<Article> findAll(){
         return postRepository.findAll();
+    }
+
+    public Article findById(Long id){
+        return postRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("not found"+id));
+    }
+
+    public void deleteById(Long id){
+        postRepository.deleteById(id);
+    }
+
+    public Article update(PostRequestDTO requestDTO,Long id){
+        Article post=postRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("not found"+id));
+        post.setTitle(requestDTO.getTitle());
+        post.setContent(requestDTO.getContent());
+        return postRepository.save(post);
     }
 }
