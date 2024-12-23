a0850d4a4610d982c74dc9a27a4086b2952f4874
1winhyun <jimking1@naver.com>
2024-12-23 15:32:17 +0900

    feat: dto 생성

---
+++ src/main/java/seunghyun/personalproject/user/dto/CreateAccessTokenRequestDTO.java
@@ -0,0 +1,16 @@
+package seunghyun.personalproject.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateAccessTokenRequestDTO {
+    private String refreshToken;
+}
