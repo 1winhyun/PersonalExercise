08adbaf7b1a8b07216710d9bc334ab8959cae5fb
1winhyun <jimking1@naver.com>
2024-12-23 14:58:32 +0900

    feat: 리프레시 토큰 엔티티, 리포지토리 생성

---
+++ src/main/java/seunghyun/personalproject/user/domain/RefreshToken.java
@@ -0,0 +1,37 @@
+package seunghyun.personalproject.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "userId",unique=true,nullable = false)
+    private Long userId;
+
+    @Column(name="refresh_token",nullable = false)
+    private String refreshToken;
+
+    public RefreshToken update(String newRefreshToken){
+        this.refreshToken=newRefreshToken;
+        return this;
+    }
+}
